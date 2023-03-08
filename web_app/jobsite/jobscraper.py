b3267c4a3cf65220d8de30252a40e8788bc442cf
dominiquemcdonald14 <123342527+dommcdonald14@users.noreply.github.com>
2023-03-08 02:28:21 -0800

    Created directory for web app files
    The directory for the web app includes:
    1. virtual environment
    2. jobscraper.py
    3.Templates folder with html templates to render

---
+++ web_app/jobsite/jobscraper.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+
+
+app = Flask(__name__)
+
+@app.route('/home')
+def home_page():
+	return render_template('home.html')
+
+
+@app.route('/recommender')
+def rec_page():
+	return render_template('recommender.html')
+
+
+@app.route('/summary')	
+def summ_page():
+		return render_template('summary.html')
+
+
+if __name__ == '__main__':
+	app.run(debug = True)
